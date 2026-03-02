ac245b172f3af08b314663e9430f0267e02be0ef
unknown <bushg5200@gmail.com>
2026-03-02 12:04:25 +0300

    added icons to searchbar & adjusted padding

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -12,10 +12,32 @@ class HomePage extends StatelessWidget {
         children: [
           Container(
             margin: EdgeInsets.only(top: 40, left: 20, right: 20),
-            decoration: BoxDecoration(),
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Color(0xff1D1617).withOpacity(0.11),
+                  blurRadius:40,
+                  spreadRadius:0.0
+                )
+              ]
+            ),
             child: TextField(
               decoration: InputDecoration(filled: true,
-              fillColor: Colors.white),
+              fillColor: Colors.white,
+              contentPadding: EdgeInsets.all(15),
+              prefixIcon: Padding(
+                padding: const EdgeInsets.all(12),
+                child: SvgPicture.asset('assets/icons/Search.svg'),
+              ),
+              suffixIcon: Padding(
+                padding: const EdgeInsets.all(8),
+                child: SvgPicture.asset('assets/icons/Filter.svg'),
+              ),
+              border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(15),
+              borderSide: BorderSide.none
+              )
+              ),
             ),
           )
         ],
@@ -43,14 +65,14 @@ class HomePage extends StatelessWidget {
       child:Container(
       margin: EdgeInsets.all(10),
       alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: Color(0xffF7F8F8),
+        borderRadius: BorderRadius.circular(10)
+      ),
       
       child: SvgPicture.asset('assets/icons/Arrow - Left 2.svg',
       height: 20,
       width: 20,
-      ),
-      decoration: BoxDecoration(
-        color: Color(0xffF7F8F8),
-        borderRadius: BorderRadius.circular(10)
       ),
 
       ),
@@ -64,14 +86,14 @@ class HomePage extends StatelessWidget {
       margin: EdgeInsets.all(10),
       alignment: Alignment.center,
       width: 37,
+      decoration: BoxDecoration(
+        color: Color(0xffF7F8F8),
+        borderRadius: BorderRadius.circular(10)
+      ),
       child: SvgPicture.asset(
         'assets/icons/dots.svg',
       height: 5,
       width: 5,
-      ),
-      decoration: BoxDecoration(
-        color: Color(0xffF7F8F8),
-        borderRadius: BorderRadius.circular(10)
       ),
 
       ),
